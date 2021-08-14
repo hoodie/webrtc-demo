18fa965c616e585d1283cf7d5fe709e4edf72215
Hendrik Sollich <hendrik@hoodie.de>
2021-08-14 12:37:01 +0200

    identify canvas streams as such

--- src/UpstreamVideo.svelte
+++ src/UpstreamVideo.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     import Details from './Details.svelte';
     import { onMount, createEventDispatcher } from 'svelte';
     const dispatch = createEventDispatcher();
@@ -7,6 +7,13 @@
     let currentActiveStream;
     $: mainTrack = currentActiveStream && currentActiveStream.getTracks()[0];
     $: upstreamName = currentActiveStream && currentActiveStream.constructor.name;
+    $: upstreamTrackName = currentActiveStream && currentActiveStream.getTracks()[0].constructor.name;
+    $: upstreamCanvas = currentActiveStream && currentActiveStream.canvas;
+    $: upstreamTrackCanvas = currentActiveStream && currentActiveStream.getTracks()[0].canvas;
+
+    const isCanvasCaptureStream = (mediaStream?: MediaStream): boolean =>
+        mediaStream?.constructor.name === 'CanvasCaptureMediaStream' ||
+        mediaStream?.getTracks()[0].constructor.name === 'CanvasCaptureMediaStreamTrack';
 
     let videoDevices = [];
     let videoStreams = [];
@@ -98,6 +105,7 @@
         transform: scale(-1, 1);
     }
 </style>
+
 <h5>upstream</h5>
 
 <video id="upstream" bind:this={upstreamVideo} autoplay="true">
@@ -124,13 +132,14 @@
             <td> <button on:click={() => makeMainStream(stream)}> {stream.name}</button></td>
             <td> <button on:click={() => stopStream(stream)}>stop</button> </td>
             <td> <button on:click={() => deleteStream(stream)}>X</button> </td>
+            <td> {#if isCanvasCaptureStream(stream)} (canvas) {/if}</td>
         </tr>
     {/each}
 </table>
 
 {#if mainTrack}
     <details>
-        <summary> {upstreamName} </summary>
+        <summary> {upstreamName} / {upstreamTrackName} / {upstreamCanvas}/ {upstreamTrackCanvas} </summary>
 
         <dl>
             <dt>readState</dt>
